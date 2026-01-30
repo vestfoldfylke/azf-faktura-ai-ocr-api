3b517f9c9318ec0c6a0d2a49dd06a8868568fed7
Rune Moskvil Lyngås <rune.moskvil.lyngas@vestfoldfylke.no>
2026-01-30 10:55:52 +0100

    minor: Export a downloadable csv with found work items in time range specified

--- src/functions/triggerExport.ts
+++ src/functions/triggerExport.ts
@@ -1,10 +1,20 @@
 import { app, type HttpRequest, type HttpResponseInit, type InvocationContext } from "@azure/functions";
+import { logger } from "@vestfoldfylke/loglady";
 import type { WithId } from "mongodb";
 
 import { getWorkItemsInDateRangeFromDb } from "../lib/mongodb-fns.js";
 
 import type { WorkItemMongo } from "../types/zod-mongo.js";
-import {logger} from "@vestfoldfylke/loglady";
+
+const convertWorkItemsToCsv = (workItems: WithId<WorkItemMongo>[]) => {
+  let csvContent = '"Fakturanummer","FraDato","FraTid","TilDato","TilTid","Timer totalt","Ansatt","Prosjekt","Aktivitet"\n';
+
+  for (const item of workItems) {
+    csvContent += `"${item.invoiceNumber}","${item.fromDate}","${item.fromTime}","${item.toDate}","${item.toTime}","${item.totalHour.toString().replace(".", ",")}","${item.employee}","${item.project ?? ""}","${item.activity ?? ""}"\n`;
+  }
+
+  return csvContent;
+}
 
 const triggerExport = async (request: HttpRequest, _context: InvocationContext): Promise<HttpResponseInit> => {
   const fromDateStr: string | null = request.query.get("fromDate");
@@ -34,12 +44,22 @@ const triggerExport = async (request: HttpRequest, _context: InvocationContext):
     toDate.toLocaleString("nb-NO", { day: "2-digit", month: "2-digit", year: "numeric", hour: "2-digit", hour12: false, minute: "2-digit", second: "2-digit" }),
     toDate.toISOString()
   );
+
   const workItems: WithId<WorkItemMongo>[] = await getWorkItemsInDateRangeFromDb(fromDate, toDate);
+
+  // add UTF-8 BOM to ensure Excel opens the file with correct encoding
+  const csvContent: string = `\uFEFF${convertWorkItemsToCsv(workItems)}`;
+  const csvName: string = `arbeidstimer_${fromDate.toISOString().slice(0, -5)}_to_${toDate.toISOString().slice(0, -5)}.csv`;
+
   logger.info("Found {WorkItemsLength} work items", workItems.length);
 
   return {
     status: 200,
-    body: JSON.stringify(workItems, null, 2)
+    headers: {
+      "Content-Disposition": `attachment; filename="${csvName}"`,
+      "Content-Type": "text/csv; charset=utf-8"
+    },
+    body: csvContent
   };
 };
 
